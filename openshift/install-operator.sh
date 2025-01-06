d692b5ed91effe545e7ac6b1803e16ef0aa7537c
Pramod Bindal <bindal.pramod@gmail.com>
2025-01-06 15:02:43 +0530

    add chains e2e

--- openshift/install-operator.sh
+++ openshift/install-operator.sh
@@ -8,7 +8,8 @@ docker run \
   quay.io/operator-framework/operator-sdk:latest \
   run bundle \
   --namespace $NAMESPACE \
-    quay.io/redhat-user-workloads/tekton-ecosystem-tenant/operator-main/bundle@sha256:df708564029c13d55efce11c5cfcdc4f4b3034c9ce8744dd9c06e19377e8b183
+    quay.io/redhat-user-workloads/tekton-ecosystem-tenant/operator-main/bundle@sha256:e06e655caad40269ab34e2ffbd48ca3ac88a021a8b82077e41b13ef5e66adcc6
+#   quay.io/redhat-user-workloads/tekton-ecosystem-tenant/operator-main/bundle@sha256:df708564029c13d55efce11c5cfcdc4f4b3034c9ce8744dd9c06e19377e8b183
 
 
  tkn version
